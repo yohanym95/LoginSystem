2f019b2daf0e6c17f413497ee925fe1e1b841f63
yohanym95 <malshikay@gmail.com>
2018-06-29 19:10:28 +0530

    added development

--- src/loginsystem/createaccount.java
+++ src/loginsystem/createaccount.java
@@ -106,7 +106,10 @@ public class createaccount extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+         NewJFrame n = new NewJFrame();
+         n.setVisible(true);
+         this.dispose();
+         
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jTextField2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField2ActionPerformed
